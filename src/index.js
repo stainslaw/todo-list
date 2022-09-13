5d47f11cdadf781739e43d76fed7e04f8a190f23
Omar-Hakim Echehamta <hakimasila@gmail.com>
2022-09-13 19:13:06 +0100

    Add scripting for three pages: Todo list, Today and This Week

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import _ from 'lodash';
+import {renderFooter} from "./footer";
+
+
+const $container = document.getElementById("container");
+
+function createHtmlElement(type, id, arrayClasses, container) {
+    const element = document.createElement(type);
+    if (id) element.id = id;
+    if (arrayClasses)arrayClasses.forEach((myClass) => element.classList.add(myClass));
+    if (container) element.innerText = container;
+
+        return element;
+}
+
+function yourList() {
+    $container.innerHTML = "";
+    renderHeader();
+    renderList();
+    renderFooter();
+}
+
+function today() {
+    $container.innerHTML = "";
+    renderHeader();
+    renderToday();
+    renderFooter();
+}
+
+function nextWeek() {
+    $container.innerHTML = "";
+    renderHeader();
+    renderWeek();
+    renderFooter();
+}
+
+yourList();
+document.addEventListener("click", (e) => {
+    const target = e.target.innerText;
+
+    if (target === "YOUR LIST") yourList();
+    if (target === "TODAY") today();
+    if (target === "NEXT WEEK") nextWeek();
+});
+
+export {createHtmlElement, $container};
