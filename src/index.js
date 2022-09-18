564eaf75fba6a2fea7e225fb849d494a33a2d500
Omar-Hakim Echehamta <hakimasila@gmail.com>
2022-09-18 12:58:09 +0100

    Comment out tomorrow and week function, along with target statements

--- src/index.js
+++ src/index.js
@@ -1,46 +1,42 @@
-import _ from 'lodash';
-import {renderFooter} from "./footer";
-
+import { renderNav } from "./navbar"
+import { renderToday } from "./today"
 
 const $container = document.getElementById("container");
 
 function createHtmlElement(type, id, arrayClasses, container) {
     const element = document.createElement(type);
     if (id) element.id = id;
-    if (arrayClasses)arrayClasses.forEach((myClass) => element.classList.add(myClass));
+    if (arrayClasses)
+      arrayClasses.forEach((myClass) => element.classList.add(myClass));
     if (container) element.innerText = container;
 
-        return element;
-}
+      return element;
+  }
 
-function yourList() {
+  function today() {
     $container.innerHTML = "";
-    renderHeader();
-    renderList();
-    renderFooter();
-}
-
-function today() {
-    $container.innerHTML = "";
-    renderHeader();
+    renderNav();
     renderToday();
-    renderFooter();
-}
+  }
 
-function nextWeek() {
-    $container.innerHTML = "";
-    renderHeader();
-    renderWeek();
-    renderFooter();
-}
+  // function week() {
+  //   $container.innerHTML = "";
+  //   renderNav();
+  //   renderWeek();
+  // }
+  //
+  // function tomorrow() {
+  //   $container.innerHTML = "";
+  //   renderNav();
+  //   renderTomorrow();
+  // }
 
-yourList();
+today();
 document.addEventListener("click", (e) => {
-    const target = e.target.innerText;
-
-    if (target === "YOUR LIST") yourList();
-    if (target === "TODAY") today();
-    if (target === "NEXT WEEK") nextWeek();
+  const target = e.target.innerText;
+  // if (target === "Today") today();
+  // if (target === "Next Week") week();
+  // if (target === "Tomorrow") tomorrow();
+  // if (target === "Settings") settings();
 });
-
-export {createHtmlElement, $container};
+export { createHtmlElement, $container };
